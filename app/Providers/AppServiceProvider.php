d45f2ecd387fd00dfdea4cf804a0776d3e22c33c
jamalludin02 <jhml1612@gmail.com>
2024-03-24 00:25:06 +0700

    sembarang

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,9 +11,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        if(config('app.env') === 'production') {
-            \URL::forceScheme('https');
-        }
     }
 
     /**
